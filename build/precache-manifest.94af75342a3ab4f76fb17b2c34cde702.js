b6465a3cb0a7bcaf1ced10334d3f54bd394daf25
Aurom Thebes <joshehenry@yahoo.com>
2019-01-12 14:14:14 -0600

    Github Pages Update

---
+++ build/precache-manifest.94af75342a3ab4f76fb17b2c34cde702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "c26d694b582fd7db51ef",
+    "url": "/Clicky-Game/static/js/main.c26d694b.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "c26d694b582fd7db51ef",
+    "url": "/Clicky-Game/static/css/main.ecd3a4f2.chunk.css"
+  },
+  {
+    "revision": "c40f32719fc4cf5381a18f26cf3fa44e",
+    "url": "/Clicky-Game/index.html"
+  }
+];
